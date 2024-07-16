d620bfbcbf9a463652f885cf9c4ed6d47c492214
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2024-07-16 14:43:57 +0300

    fixed angular material table

--- frontend/src/app/transactions.service.ts
+++ frontend/src/app/transactions.service.ts
@@ -6,36 +6,44 @@ import { Injectable } from '@angular/core';
 export class TransactionsService {
   url = 'http:localhost:8000/api'
   constructor() { }
-  async getTransactions():Promise<Array<Transaction>>{
-    let transactions:Transaction[] = [
+  async getTransactions(): Promise<Array<Transaction>> {
+    let transactions: Transaction[] = [
       {
-        id:'1',
-        amount:100,
+        id: '1',
+        amount: 100,
+        sent: false,
+        sent_on: 'today'
       },
       {
-        id:'2',
-        amount:200,
+        id: '2',
+        amount: 200,
+        sent: false,
+        sent_on: 'today'
       },
       {
-        id:'3',
-        amount:300,
+        id: '3',
+        amount: 300,
+        sent: false,
+        sent_on: 'today'
       },
     ]
     return transactions
   }
-  async cancelTransaction(id:string):Promise<boolean>{
+  async cancelTransaction(id: string): Promise<boolean> {
     return true
   }
-  async sendTransaction(destination:string,amount: any):Promise<boolean>{
+  async sendTransaction(destination: string, amount: any): Promise<boolean> {
     console.log(amount)
     return true
   }
-  async rejectTransaction(id:string):Promise<boolean>{
+  async rejectTransaction(id: string): Promise<boolean> {
     return true
   }
 }
-export class Transaction {
-  id: string = '';
-  amount: number = .0;
+export interface Transaction {
+  id: string;
+  amount: number;
+  sent: boolean;
+  sent_on: string;
 }
 
--- frontend/src/app/transactions/transactions.component.ts
+++ frontend/src/app/transactions/transactions.component.ts
@@ -3,19 +3,27 @@ import { NgFor } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { TransactionsService } from '../transactions.service';
 import { Transaction } from "../transactions.service";
+import { MatButtonModule } from '@angular/material/button'
+import { MatTableModule } from '@angular/material/table'
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [NgFor,CommonModule],
+  imports: [
+    NgFor,
+    CommonModule,
+    MatButtonModule,
+    MatTableModule
+  ],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css'
 })
 export class TransactionsComponent {
-  transactions:Transaction[] = []
-  trnsactionsService:TransactionsService = inject(TransactionsService)
-  constructor(){
-    this.trnsactionsService.getTransactions().then((data)=>{
-      this.transactions=data
+  transactions: Transaction[] = []
+  displayedColumns: string[] = ['id', 'amount', 'sent_on']
+  trnsactionsService: TransactionsService = inject(TransactionsService)
+  constructor() {
+    this.trnsactionsService.getTransactions().then((data) => {
+      this.transactions = data
     })
   }
 }
